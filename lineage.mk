0385c3844e716eda95120b17af23767b8fbc605d
droidthug <saivishal2001@gmail.com>
2017-03-08 16:27:01 +0000

    lineage: correct vendor path

--- lineage.mk
+++ lineage.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Device display
 TARGET_SCREEN_HEIGHT := 1920
